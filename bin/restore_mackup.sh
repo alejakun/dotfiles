dbca5854cbb7f2588053ff2c4b3eca9b1d07cfcd
Alex Pedroza <alejandro.pedroza@bwr.mx>
2022-05-26 15:45:32 -0500

    Added validation to skip Mackup Backup

--- bin/restore_mackup.sh
+++ bin/restore_mackup.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-# Warn before mackup restoration
-wait_for_confirmation "Confirm Dropbox Mackup folder is already synched"
+ask_for_confirmation_input "Do you wish to restore Mackup Backup?"
 
-echo "🚛 Moving Mackup configuration files"
-
-# Symlink the Mackup config file to the home directory
-rm -rf "$HOME/.mackup.cfg"
-ln -s "$DOTFILES/macos/mackup/mackup.cfg" "$HOME/.mackup.cfg"
-rm -rf "$HOME/.mackup"
-ln -s "$DOTFILES/macos/mackup/apps" "$HOME/.mackup"
-
-mackup restore
-
-# Restore the OmniApps Templates
-ask_for_confirmation_input "Do you wish to to restore OmniGraffle 7 Templates?"
 if test $confirm = true; then
-    # Open the app to be sure the directories exist
-    open_app "OmniGraffle"
-    sleep 2
-    killall "OmniGraffle"
+    # Warn before mackup restoration
+    wait_for_confirmation "Confirm Dropbox Mackup folder is already synched"
 
-    cp -rf "$HOME/Dropbox/Mackup/Library/Containers/com.omnigroup.OmniGraffle7.MacAppStore/Data/Library/Application Support/The Omni Group/OmniGraffle/" \
-           "$HOME/Library/Containers/com.omnigroup.OmniGraffle7.MacAppStore/Data/Library/Application Support/The Omni Group/OmniGraffle/"
+    echo "🚛 Moving Mackup configuration files"
+
+    # Symlink the Mackup config file to the home directory
+    rm -rf "$HOME/.mackup.cfg"
+    ln -s "$DOTFILES/macos/mackup/mackup.cfg" "$HOME/.mackup.cfg"
+    rm -rf "$HOME/.mackup"
+    ln -s "$DOTFILES/macos/mackup/apps" "$HOME/.mackup"
+
+    mackup restore
+
+    # Restore the OmniApps Templates
+    ask_for_confirmation_input "Do you wish to restore OmniGraffle 7 Templates?"
+    if test $confirm = true; then
+        # Open the app to be sure the directories exist
+        open_app "OmniGraffle"
+        sleep 10
+        killall "OmniGraffle"
+
+        cp -rf "$HOME/Dropbox/Mackup/Library/Containers/com.omnigroup.OmniGraffle7.MacAppStore/Data/Library/Application Support/The Omni Group/OmniGraffle/" \
+              "$HOME/Library/Containers/com.omnigroup.OmniGraffle7.MacAppStore/Data/Library/Application Support/The Omni Group/OmniGraffle/"
+    fi
+else
+    echo "⏭ Skipping Mackup restore"
 fi
