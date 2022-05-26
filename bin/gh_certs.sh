1b3bc19ade86df9f9e802f45292c54e56de9d8ef
Alex Pedroza <alejandro.pedroza@bwr.mx>
2022-05-26 11:08:41 -0500

    Changed handling of directories to use quotes and updated echo spaces

--- bin/gh_certs.sh
+++ bin/gh_certs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Generating a new SSH key for GitHub..."
+echo "💲  Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
@@ -17,4 +17,4 @@ ssh-add -K ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
-echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub: https://github.com/settings/keys"
+echo "💲  run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub: https://github.com/settings/keys"
