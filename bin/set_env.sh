6c0fa861910e9c8e6befa5cb6b324d7cb412639b
Alex Pedroza <alejandro.pedroza@bwr.mx>
2024-05-10 12:35:20 -0600

    Changed handling of variables from .zshenv to directly exporting and pointed to new files

--- bin/set_env.sh
+++ bin/set_env.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-source $HOME/.dotfiles/zsh/.zshenv
-source $HOME/.dotfiles/private/authinfo
-source $HOME/.dotfiles/bin/scripts/utils
+export DOTFILES=$HOME/.dotfiles
+export ZSH=$HOME/.oh-my-zsh
+export PATH=$DOTFILES/bin/scripts:$DOTFILES/bin/scripts/utils:$PATH
+
+source "$HOME/.dotfiles/private/authinfo"
+source "$DOTFILES/bin/scripts/utils"
