40855d4baf82a6e5e903ea98e3f1a2da1c6924f7
Alex Pedroza <alejandro.pedroza@bwr.mx>
2024-05-14 09:32:55 -0600

    Added HOMEBREW_BREWFILE and MACOS_VERSION environment variables

--- bin/set_env.sh
+++ bin/set_env.sh
@@ -3,6 +3,8 @@
 export DOTFILES=$HOME/.dotfiles
 export ZSH=$HOME/.oh-my-zsh
 export PATH=$DOTFILES/bin/scripts:$DOTFILES/bin/scripts/utils:$PATH
+export HOMEBREW_BREWFILE=$DOTFILES/brew/Brewfile
+export MACOS_VERSION=$(sw_vers | grep ProductVersion | grep -o -E -i '([0-9]*[.])?[0-9]+.*')
 
 source "$HOME/.dotfiles/private/authinfo"
 source "$DOTFILES/bin/scripts/utils"
